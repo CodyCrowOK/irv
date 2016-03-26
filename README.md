f0ae8ab1fe0a43f65c1816c73aadecf9db87803a
Cody Crow <cody@cms07.org>
2016-03-26 13:15:23 -0500

    Finished vote counter

---
+++ README.md
@@ -0,0 +1,19 @@
+# Instant-runoff voting
+
+Ballot counter for IRV in Racket.
+
+USAGE:
+
+	(read-votes-file "filename.csv")
+
+CSV file format:
+
+	Candidate 1,Candidate 2,Candidate 3,Candidate 4
+    3,2,4,1
+    1,2,3,4
+    2,1,3
+    4
+    ...
+
+The first line of the CSV holds the candidates' names. Each subsequent line is a ballot in order of preference (e.g. `2,1,3` is a ballot for Candidate 2, Candidate 1, and Candidate 3, in that order).
+
--- irv.rkt
+++ irv.rkt
@@ -1,26 +1,52 @@
 #lang racket
 
-(require srfi/1)  ; import `list-index`
-(require srfi/26) ; import `cute`
+(require srfi/1)  ; `list-index`
+(require srfi/26) ; `cute`
+(require 2htdp/batch-io) ; `read-csv-file`
 
 ; IRV ballot counter
 
-; Testing data
-(define candidates (list "Lenin" "Debs" "Roosevelt" "Reagan"))
-(define votes
-  (list
-   (list 1 2 3)
-   (list 2 3 1)
-   (list 4)
-   (list 4)
-   (list 4)
-   (list 4 3 2)
-   (list 4 1 3)
-   (list 3 1 2)))
-
 ; Main function
+(define (read-votes-file filename)
+  (find-winner (get-votes filename) (get-candidates filename)))
+
+; Gets the candidates, which should be the first line of the csv
+(define (get-candidates filename)
+  (car (read-csv-file filename)))
+
+; Gets the ballots from csv and converts them to usable form
+(define (get-votes filename)
+  (map (lambda (xs) (map string->number xs)) (cdr (read-csv-file filename))))
+
+; Return election winner
+(define (find-winner ballots candidates)
+  (get-element (count-votes ballots) candidates))
+
+; Get nth element from xs
+(define (get-element n xs)
+  (if (zero? n)
+      (car xs)
+      (get-element (- n 1) (cdr xs))))
+
+; Move through the rounds until a winner is found
 (define (count-votes ballots)
-  (+ 1 0))
+  (if (winner-exists (tally-round ballots))
+      (max-position (tally-round ballots))
+      (count-votes (remove-loser ballots))))
+
+;(define (remove-loser ballots)
+;  (map (lambda (xs)
+;         (if (equal? (car xs) (min-position (tally-round ballots)))
+;             (cdr xs)
+;             xs)) ballots))
+
+(define (remove-loser ballots)
+  (map remove-head-if-loser ballots `((min-position (tally-round ballots)))))
+
+(define (remove-head-if-loser ballot loser)
+  (if (equal? loser (car ballot))
+      (cdr ballot)
+      ballot))
 
 ; Tallies each round
 (define (tally-round ballots)
@@ -56,7 +82,12 @@
 (define (is-winner x sum)
   (> x (/ sum 2)))
 
-; Find position of the lowest element in a list
+; Find position of the smallest element in a list
 (define (min-position list)
   (and (not (null? list))
        (list-index (cute = (apply min list) <>) list)))
+
+; Find position of the largest element in a list
+(define (max-position list)
+  (and (not (null? list))
+       (list-index (cute = (apply max list) <>) list)))
